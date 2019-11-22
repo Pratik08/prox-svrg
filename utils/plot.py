f7215e34a7727b6cb8b30883a2df86442623e48f
shashwatverma14@gmail.com <shashwatverma14@gmail.com>
2019-11-22 02:32:38 -0500

    add plotting interface

---
+++ utils/plot.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+def plot_num_non_zeros(stats):
+
+    nnzs = []
+    effective_passes = []
+    og = []
+
+    for stat in stats:
+        nnz = stat.num_non_zeros
+        nnzs.append(nnz)
+
+        ep = stat.effective_passes
+        eps.append(ep)
+
+        og = stat.objective_gaps
+        ogs.append(og)
+
+    # plot nnzs
+
+    # plot eps
+
+    # plot ogs
