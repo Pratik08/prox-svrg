b0fc1ffaff167cf164bcaf59036ef58a223c62d6
Pratik Dubal <pratik.dubal08@gmail.com>
2019-11-22 03:28:38 -0500

    Added dataloader for covertype.

--- test/test_dataset_loader.py
+++ test/test_dataset_loader.py
@@ -2,9 +2,11 @@ import sys
 sys.path.append('../utils')
 
 import torch
-from dataset import Rcv1, Sido
+from dataset import Rcv1, Covertype, Sido
 
 rcv1 = Rcv1()
 print('Loaded Rcv1')
+covertype = Covertype()
+print('Loaded Covertype')
 sido = Sido()
 print('Loaded Sido')
