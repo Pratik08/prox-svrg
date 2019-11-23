85acb9d261d22908866238b2d182b0764a6518b0
Saurabh Sharma <saurabhsharma@Saurabhs-MacBook-Pro.local>
2019-11-22 19:15:41 -0500

    Changed stochastic update in Prox-SVRG

--- test/test_optimizers.py
+++ test/test_optimizers.py
@@ -5,18 +5,21 @@ sys.path.append('../')
 from losses import *
 from optimizers import *
 from utils.dataset import Sido,Covertype
-
-covertype = Covertype()
-X = covertype.X
-y = covertype.y
+num_features = 50
+num_examples = 1000
+sido = Sido()
+X = torch.narrow(sido.X,dim=1,start=0,length=num_features)
+X = torch.narrow(X,dim=0,start=0,length=num_examples)
+print(X.size())
+y = torch.narrow(sido.y,dim=0,start=0,length=num_examples)
 
 
 optimizer = ProxSVRGOptimizer()
 hp = dict()
-hp['max_iter'] = 100
-hp['lr'] = 1e-5
+hp['max_iter'] = 300
+hp['lr'] = 1e-3
 hp['s'] = 10
-hp['m'] = 10000
-hp['eta'] = 1e-4
-hp['coeff'] = {'l1':0.0001, 'l2':0.0001}
+hp['m'] = X.size(0)*2
+hp['eta'] = 1e-1
+hp['coeff'] = {'l1':1e-4, 'l2':1e-4}
 optimizer.optimize(X,y,hp,log_loss,elastic_net_regularizer,prox_loss)
