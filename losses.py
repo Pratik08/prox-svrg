f531379f751bd50f503ff16657cc111842ee0713
Pratik Dubal <pratik.dubal08@gmail.com>
2019-11-23 21:33:20 -0500

    Fixed issues in losses.

--- losses.py
+++ losses.py
@@ -13,13 +13,15 @@ class mse():
 class log_loss():
     def compute(X, y, w):
         return torch.mean(torch.log(1. + torch.exp(-1. * torch.mul(y,
-                          torch.sum(torch.mul(X, w), dim=1)))))
+                          torch.sum(torch.mul(X, w), dim=1))))).double()
 
     def grad(X, y, w):
-        y_hat = torch.sum(torch.mul(X, w), axis=1)
-        t1 = torch.div(torch.exp(-1. * torch.mul(y, y_hat)), 1. + torch.exp(-1. * torch.mul(y, y_hat)))
-        t2 = torch.mul(-1. * y.repeat(1, X.size(1)).reshape(X.size(1), y.size(0)), X.t())
-        w_ret = torch.div(torch.mul(t2, t1).sum(axis=1), X.size(0))
+        y_hat = torch.sum(torch.mul(X, w), axis=1).double()
+        t1 = torch.div(torch.exp(-1. * torch.mul(y, y_hat)),
+                       1. + torch.exp(-1. * torch.mul(y, y_hat))).double()
+        t2 = torch.mul(-1. * y.repeat(1, X.size(1)).reshape(X.size(1),
+                       X.size(0)), X.t()).double()
+        w_ret = torch.div(torch.mul(t2, t1).sum(axis=1), X.size(0)).double()
         return w_ret
 
 
@@ -34,12 +36,14 @@ class prox_loss():
 class l1_regularizer():
     def compute(w, coeff):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        return torch.mul(torch.dist(w, torch.zeros(w.size()).double().to(device), p=1),
+        return torch.mul(torch.dist(w,
+                         torch.zeros(w.size()).double().to(device), p=1),
                          0.5*coeff['l1']).to(device)
 
     def grad(w, coeff):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        if torch.dist(w.double(), torch.zeros(w.size()).double().to(device), p=1) >= 0:
+        if torch.dist(w.double(),
+                      torch.zeros(w.size()).double().to(device), p=1) >= 0:
             return torch.mul(torch.ones(w.size()).to(device),
                              0.5*coeff['l1']).to(device)
         else:
@@ -50,8 +54,8 @@ class l1_regularizer():
 class l2_regularizer():
     def compute(w, coeff):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        return torch.mul(torch.dist(w, torch.zeros(w.size()).double(), p=2).to(device),
-                                    coeff['l2'])
+        return torch.mul(torch.dist(w, torch.zeros(w.size()).double(),
+                         p=2).to(device), coeff['l2'])
 
     def grad(w, coeff):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -67,12 +71,14 @@ class elastic_net_regularizer():
         return torch.add(l1_regularizer.grad(w, coeff),
                          l2_regularizer.grad(w, coeff))
 
+
 class loss_plus_regulalizer():
     def compute(X, y, w, coeff, loss, regularizer):
         if regularizer is not None:
-            return loss.compute(X,y,w) + regularizer.compute(w,coeff)
-        return loss.compute(X,y,w)
+            return loss.compute(X, y, w) + regularizer.compute(w, coeff)
+        return loss.compute(X, y, w)
+
     def grad(X, y, w, coeff, loss, regularizer):
         if regularizer is not None:
-            return loss.grad(X,y,w) + regularizer.grad(w,coeff)
-        return loss.grad(X,y,w)
+            return loss.grad(X, y, w) + regularizer.grad(w, coeff)
+        return loss.grad(X, y, w)
